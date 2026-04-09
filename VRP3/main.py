c292f6d646ad42759fa460912cdd1a84b5eade8b
grzesiex22 <grzesiug2002@o2.pl>
2026-04-09 23:49:08 +0200

    Ostatnia poprawka stylistyczna

--- VRP3/main.py
+++ VRP3/main.py
@@ -1,21 +1,20 @@
 from colorama import Fore, Back, Style, init
 
-
 from VRP3.Generator import Generator
 from VRP3.VRP import VRP
 from VRP3.ACO_for_VRP import ACO_for_VRP
 from VRP3.Visualizer import Visualizer, plt
-from VRP3.VRP_solver import solve_vrp
 from VRP3.Gready import greedy_vrp
 
 
 VISUALIZE = True
 
+
 def main():
     # Inicjalizacja colorama (wymagana na Windowsie, by kody działały)
     init(autoreset=True)
 
-    # 1️⃣ generowanie klientów
+    # 1 generowanie klientów
     generator = Generator(d0=10, d1=100, t0=0, t1=5, n=5, seed=54)
     # generator = Generator(d0=10, d1=100, t0=0, t1=5, n=5, seed=50)
 
@@ -39,10 +38,10 @@ def main():
 
     print(Fore.LIGHTYELLOW_EX + "#" * 118 + Style.RESET_ALL)
 
-    # 2️⃣ stworzenie problemu VRP
+    # 2 stworzenie problemu VRP
     problem = VRP(nodes, vehicles=vehicles)
 
-    # 3️⃣ uruchomienie algorytmu mrówkowego
+    # 3 ACO
     print("\n" + Fore.MAGENTA + Style.BRIGHT + "#" * 118)
     print(f"{'ALGORYTM MRÓWKOWY':^118}")
     print(Fore.MAGENTA + Style.BRIGHT + "#" * 118 + "\n" + Style.RESET_ALL)
@@ -51,7 +50,7 @@ def main():
 
     aco_vehicles, ACO_cost = aco.run(patience=300)
 
-    # 4️⃣ wyniki
+    # - wyniki
     print("\nWłasne VRP - Najlepsza trasa:")
     best_route = [v.route for v in aco_vehicles]
     indices = [[node.id for node in route] for route in best_route]
@@ -61,19 +60,21 @@ def main():
 
     print(f"\nWłasny czas trasy:" + Fore.YELLOW + f"{ACO_cost/60} minut" + Style.RESET_ALL)
 
-    # Wyświetlenie szczegółów
+    # - Wyświetlenie szczegółów
     aco_ok = problem.print_summary(aco_vehicles)
 
     if VISUALIZE:
         visualizer = Visualizer(nodes)
         visualizer.show(best_route, title="WŁASNY")
 
-    # 5 GREEDY
+    # 4 GREEDY
     print("\n" + Fore.MAGENTA + Style.BRIGHT + "#" * 118)
     print(f"{'ALGORYTM GREADY':^118}")
     print(Fore.MAGENTA + Style.BRIGHT + "#" * 118 + Style.RESET_ALL)
 
     greedy_vehicles, GREEDY_cost = greedy_vrp(nodes, problem.time_matrix_seconds, problem.vehicles)
+
+    # - wyniki
     print("\nGready  - VRP  - Najlepsza trasa:")
     optimal_routes = [v.route for v in greedy_vehicles]
     indices = [[node.id for node in route] for route in optimal_routes]
@@ -83,14 +84,14 @@ def main():
 
     print(f"\nGREEDY czas trasy:" + Fore.YELLOW + f"{GREEDY_cost/60} minut" + Style.RESET_ALL)
 
-    # Wyświetlenie szczegółów
+    # - Wyświetlenie szczegółów
     greedy_ok = problem.print_summary(greedy_vehicles)
 
     if VISUALIZE:
         visualizer = Visualizer(nodes)
         visualizer.show(optimal_routes, title="GREEDY")
 
-    # --- PORÓWNANIE ---
+    # 5 PORÓWNANIE
     print("\n" + Fore.LIGHTGREEN_EX + "#" * 118)
     print(f"{'PORÓWNANIE':^118}")
     print("-" * 118 + Style.RESET_ALL)
@@ -99,7 +100,6 @@ def main():
     # print(f"OR-Tools cost: {GREEDY_cost / 60} minut")
     style_greedy_ok = Fore.GREEN if greedy_ok else Fore.RED
     print(f"GREEDY czas trasy:" + style_greedy_ok + f"{GREEDY_cost/60} minut" + Style.RESET_ALL)
-
     print(Fore.LIGHTGREEN_EX + "#" * 118 + Style.RESET_ALL)
 
 
